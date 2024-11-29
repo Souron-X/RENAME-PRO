d2dbe7d91dbd5413c7805ffef4f58cd802248610
Souron <souravbachhar560@gmail.com>
2024-11-29 22:49:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7865697303:AAFpJfdFpS8nre-npWS5ANli7pamvyjQGJM")
+API_ID = int(os.environ.get("API_ID", "25917332"))
+API_HASH = os.environ.get("API_HASH", "e602b8e0ec123b7d58dcb70c31703664")
 OWNER = int(os.environ.get("OWNER", "5897395932"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "SouroRenameBot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "AmorousSouls")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002405642627"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://orionoshi:seByEMSqlnnlmWKc@cluster0.13mls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
